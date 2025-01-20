2500ae4ae98e0fbe35e2d672780fcb6aec424ded
Shivaraj Nayak <shivarajatmk@gmail.com>
2025-01-20 11:58:21 +0530

    Authorization

--- src/app.js
+++ src/app.js
@@ -1,38 +1,17 @@
 const express = require("express");
 const app  = express();
 
+const {adminAuth} = require("./middleware/adminAuth");
 
+app.use("/admin", adminAuth);
 
-app.use(
-    "/user",
-    (req, res, next)=>{
-    //Route handler
-    console.log("Handling the route handler!!");
-    //res.send("Response!!");
-    next();
-   },
-   (req, res, next)=>{
-    console.log("Handling the route user 2!!");
-    res.send("2nd Response!!!");
-    next();
-   },
-   (req, res, next)=>{
-    console.log("Handling the route user 3!!");
-    res.send("3nd Response!!!");
-    next();
-   },
-   (req, res, next)=>{
-    console.log("Handling the route user 4!!");
-    res.send("4nd Response!!!");
-    next();
-   },
-   (req, res, next)=>{
-    console.log("Handling the route user 5!!");
-    res.send("5nd Response!!!");
-    next();
-   }
+app.get("/admin/getAllData", (req, res) =>{
+    res.send("All data sent");
+});
 
-);
+app.delete("/admin/deleteUser", (req, res) =>{
+    res.send("Deleted the user");
+});
 
 app.listen(3000, ()=> {
     console.log("Server is successfully listening on server 3000...");
