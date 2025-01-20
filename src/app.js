2204cd8771dee86eb6f373579a70c215a6b9c9a1
Shivaraj Nayak <shivarajatmk@gmail.com>
2025-01-20 10:18:51 +0530

    Handling the multiple route Handlers

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const app  = express();
 
-app.use("/test", (req, res)=>{
-    res.send("Hello from the server");
-});
 
-app.use("/hello", (req, res)=>{
-    res.send("Hello hello hello!!!");
-});
 
-app.use("/", (req, res)=>{
-    res.send("Hello from the dashboard");
-});
+app.use(
+    "/user",
+    (req, res, next)=>{
+    //Route handler
+    console.log("Handling the route handler!!");
+    //res.send("Response!!");
+    next();
+   },
+   (req, res, next)=>{
+    console.log("Handling the route user 2!!");
+    res.send("2nd Response!!!");
+    next();
+   },
+   (req, res, next)=>{
+    console.log("Handling the route user 3!!");
+    res.send("3nd Response!!!");
+    next();
+   },
+   (req, res, next)=>{
+    console.log("Handling the route user 4!!");
+    res.send("4nd Response!!!");
+    next();
+   },
+   (req, res, next)=>{
+    console.log("Handling the route user 5!!");
+    res.send("5nd Response!!!");
+    next();
+   }
+
+);
 
 app.listen(3000, ()=> {
     console.log("Server is successfully listening on server 3000...");
--- src/homework.md
+++ src/homework.md
@@ -9,3 +9,11 @@ Install nodemon and upadte scripts inside package.json
 what are dependencies
 what is the use of "-g" while npm install
 Differences between caret and tilde (^ vs ~)
+
+Intialize the git in our repository
+create .gitignore file to ignore the node_moduls and other files and folders
+
+create a remote repo on GitHub
+
+push all code to remote origin
+
